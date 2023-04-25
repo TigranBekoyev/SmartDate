de0541b827016cd1602c25cb94971224a896c577
Tigran <tigran.shpak@mail.ru>
2023-04-25 21:11:31 +0300

    Class moved to header file

--- SmartDate.cpp
+++ SmartDate.cpp
@@ -2,26 +2,8 @@
 *  Проект "Класс умная дата"
 */
 #include <iostream>
+#include "SmartDate.h"
 
-class SmartDate
-{
-private:
-	short day;
-	short month;
-	short year;
-public:
-	SmartDate(short _d, short _m, short _y);
-
-
-};
-
-SmartDate::SmartDate(short _d, short _m, short _y)
-{
-	day = _d;
-	month = _m;
-	year = _y;
-
-}
 
 int main()
 {
---
+++ SmartDate.h
@@ -0,0 +1,21 @@
+#pragma once
+
+class SmartDate
+{
+private:
+	short day;
+	short month;
+	short year;
+public:
+	SmartDate(short _d, short _m, short _y);
+
+
+};
+
+SmartDate::SmartDate(short _d, short _m, short _y)
+{
+	day = _d;
+	month = _m;
+	year = _y;
+
+}
